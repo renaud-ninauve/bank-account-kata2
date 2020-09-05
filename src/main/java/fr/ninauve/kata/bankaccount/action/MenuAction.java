c1e5a65b27b0c99c6a4a56bcc202592d0b9e22d9
renaud-ninauve <renaud.ninauve.92@gmail.com>
2020-09-05 11:50:35 +0200

    [story-1][green] Exit menu

--- src/main/java/fr/ninauve/kata/bankaccount/action/MenuAction.java
+++ src/main/java/fr/ninauve/kata/bankaccount/action/MenuAction.java
@@ -4,6 +4,9 @@ import fr.ninauve.kata.bankaccount.io.Console;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Objects;
+
 @Component
 public class MenuAction implements Action {
 
@@ -18,6 +21,18 @@ public class MenuAction implements Action {
     @Override
     public Action execute() {
 
+        console.printLines(MessageConstants.MENU);
+        final String input = console.waitAndGetInput();
+        if (!Objects.equals(input, "2")) {
+            return badInput();
+        }
+
         return null;
+    }
+
+    private Action badInput() {
+
+        console.printLines(List.of(MessageConstants.BAD_INPUT_MENU));
+        return this;
     }
 }
